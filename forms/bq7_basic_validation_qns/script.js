f1a1effc6cb5583861668dd8df50d5a2dbc7a06c
Wesley Cheah <wesley_188@yahoo.com>
2022-05-27 02:19:43 +0000

    To coontinue with bq7

---
+++ forms/bq7_basic_validation_qns/script.js
@@ -0,0 +1,18 @@
+document.querySelector('#submit')
+.addEventListener('click', function() {
+    // Set flags
+    // 1. number of scoops
+    // -> number of scoops must be between 1 to 5
+    let invalidScoopNumber = false;
+    // -> number of scoops must be whole number (integer)
+    let scoopNotANumber = false;
+
+    // 2. number of toppinngs
+    // -> number of toppings must not exceed 3
+    let tooMuchToppings = false;
+
+    // 
+
+    
+    // Select textbox #number-of-scoop
+});
