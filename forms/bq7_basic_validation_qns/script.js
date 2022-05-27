d47ff983bdf2217883a4b26c43866c6894ba5844
Wesley Cheah <wesley_188@yahoo.com>
2022-05-27 13:54:42 +0000

    Commpleted bq7 but test failed

--- forms/bq7_basic_validation_qns/script.js
+++ forms/bq7_basic_validation_qns/script.js
@@ -7,12 +7,40 @@ document.querySelector('#submit')
     // -> number of scoops must be whole number (integer)
     let scoopNotANumber = false;
 
-    // 2. number of toppinngs
+    // 2. number of toppings
     // -> number of toppings must not exceed 3
     let tooMuchToppings = false;
-
-    // 
-
-    
+ 
     // Select textbox #number-of-scoop
+    // -> cast to integer (whole number)
+    let numberOfScoop = parseInt(document.querySelector('#number-of-scoop').value);
+
+    if (!numberOfScoop || numberOfScoop < 1 || numberOfScoop > 5) {
+        invalidScoopNumber = true;
+    }
+
+    // Select all checked checkboxes (toppings)
+    let selectedToppings = [];
+    let checkboxes = document.querySelectorAll('.toppings');
+    for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+            selectedToppings.push(checkbox.value);
+        }
+    }
+
+    if (selectedToppings.length > 3) {
+        tooMuchToppings = true;
+    }
+
+    // Error string
+    let errorArr = [];
+    if (invalidScoopNumber) {
+        errorArr.push('invalid-scoop-number');
+    }
+    if (tooMuchToppings) {
+        errorArr.push('too-much-toppings');
+    }
+
+    console.log(errorArr);
+
 });
